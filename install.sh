92bcd72c75163ceda193d936f8cb95a567a78888
detsch75 <schuetzd@web.de>
2021-11-23 10:57:55 +0100

    Fix docker start

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ if [[ $(getent passwd squeezeboxserver) = "" ]]; then
     useradd -M -s /usr/sbin/nologin -c "Squeezebox Server user" squeezeboxserver
 fi
 
-cat > /etc/systemd/system/docker.logitechmediaserver.service << 'EOF'
+user_id=`id -u squeezeboxserver`
+
+cat > /etc/systemd/system/docker.logitechmediaserver.service << EOF
 [Unit]
 Description=LogitechMediaServer Service
 After=docker.service
@@ -15,7 +17,7 @@ TimeoutStartSec=0
 Restart=always
 ExecStartPre=-/usr/bin/docker exec logitechmediaserver stop
 ExecStartPre=-/usr/bin/docker rm -f logitechmediaserver
-ExecStart=/usr/bin/docker run -d -u squeezeboxserver -p 9000:9000 -p 9090:9090 -p 3483:3483 -p 3483:3483/udp -v /home/logitechmediaserver/server:/srv/squeezebox -v /home/logitechmediaserver/music:/srv/music -v /home/logitechmediaserver/playlists:/srv/playlists --name logitechmediaserver logitechmediaserver
+ExecStart=/usr/bin/docker run -d -u -u $user_id -p 9000:9000 -p 9090:9090 -p 3483:3483 -p 3483:3483/udp -v /home/logitechmediaserver/server:/srv/squeezebox -v /home/logitechmediaserver/music:/srv/music -v /home/logitechmediaserver/playlists:/srv/playlists --name logitechmediaserver logitechmediaserver
 ExecStop=/usr/bin/docker exec logitechmediaserver stop
  
 [Install]
